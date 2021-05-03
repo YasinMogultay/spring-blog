e6d74fc6250c1caa1d16b5d706434b223d69d43f
Yasin Mogultay <yasin.mglty@gmail.com>
2021-05-03 20:22:35 +0300

    done with post exercise

--- src/main/java/com/example/demo/controller/PostController.java
+++ src/main/java/com/example/demo/controller/PostController.java
@@ -1,32 +1,40 @@
 package com.example.demo.controller;
 
+import com.example.demo.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @Controller
 public class PostController {
 
     @GetMapping("/posts")
-    @ResponseBody
-    public String postsMethod () {
-        return "posts index page";
+    public String postsMethod (Model model) {
+        ArrayList<Post> post = new ArrayList<>();
+        post.add(new Post(1,"First Post Title","First Post Body"));
+        post.add(new Post(2,"Second Post Title","Second Post Body"));
+        post.add(new Post(3,"Third Post Title","Third Post Body"));
+        model.addAttribute("posts", post);
+        return "posts/index";
     }
 
-    @RequestMapping( path = "/posts/{id}", method = RequestMethod.GET)
-    @ResponseBody
-    public String postIdd (@PathVariable int id) {
-        return "Your post id is " + id;
+    @GetMapping("/posts/{id}")
+    public String postId (@PathVariable int id, Model model) {
+        Post post = new Post(id,"Post Title","Post Body");
+        model.addAttribute("id", id);
+        model.addAttribute("post", post);
+        return "posts/show";
     }
 
     @GetMapping("/posts/create")
-    @ResponseBody
     public String createPost () {
         return "FORM";
     }
 
-    @RequestMapping( path = "/posts/create", method = RequestMethod.POST)
-    @ResponseBody
-    public String postIdd () {
+    @PostMapping( path = "/posts/create")
+    public String postId () {
         return "Your post will be submiting";
     }
 }
