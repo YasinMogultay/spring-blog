7fd6de5920eeec6652c2ab7eefbb0809850e8b10
Yasin Mogultay <yasin.mglty@gmail.com>
2021-05-06 17:03:16 +0300

    adjusted

--- src/main/java/com/example/demo/repositories/PostRepository.java
+++ src/main/java/com/example/demo/repositories/PostRepository.java
@@ -3,5 +3,5 @@ package com.example.demo.repositories;
 import com.example.demo.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post,Long> { //always extend from JpaRepository for injection with object that you have and type of your id
+public interface PostRepository extends JpaRepository<Post, Long> { //always extend from JpaRepository for injection with object that you have and type of your id
 }
