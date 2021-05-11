b1ad051f4dae068fbdd44fc4e1f3b0306f8dc318
Yasin Mogultay <yasin.mglty@gmail.com>
2021-05-11 22:46:54 +0300

    updated PostRepo to be avoid any error cause of missing close curly brace

--- src/main/java/com/example/demo/repositories/PostRepository.java
+++ src/main/java/com/example/demo/repositories/PostRepository.java
@@ -6,4 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post,Long> { //always extend from JpaRepository for injection with object that you have and type of your id
     Post findByTitle(String title);
-
+}
