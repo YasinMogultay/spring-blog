b19949d3838e942bb708d9ea9206be26fa237447
Yasin Mogultay <yasin.mglty@gmail.com>
2021-05-11 22:29:47 +0300

    feat:completed test integrations exercise

--- src/main/java/com/example/demo/repositories/PostRepository.java
+++ src/main/java/com/example/demo/repositories/PostRepository.java
@@ -4,4 +4,5 @@ import com.example.demo.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post,Long> { //always extend from JpaRepository for injection with object that you have and type of your id
+    Post findByTitle(String title);
 }
